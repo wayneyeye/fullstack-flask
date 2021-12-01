8afb8cd2caef49ab43de8be001ac79ca9e63efd8
Wenhe Ye <wenhe.ye@adevinta.com>
2021-12-02 06:23:10 +0800

    add nav bar; url_for and simple if else

--- application/routes.py
+++ application/routes.py
@@ -4,4 +4,16 @@ from flask import render_template
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",login=False)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
