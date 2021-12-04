e07962309293dbb74409f7ccef32aed963955dbb
Wenhe Ye <wenhe.ye@adevinta.com>
2021-12-04 16:10:18 +0800

    add layout.html

--- application/routes.py
+++ application/routes.py
@@ -4,7 +4,7 @@ from flask import render_template
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html",login=False)
+    return render_template("index.html",login=True)
 
 @app.route("/login")
 def login():
