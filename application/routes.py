0f297c8af2f844f9ebfa01c4c2cbaaf833f7088c
Wenhe Ye <wenhe.ye@adevinta.com>
2021-12-25 07:11:08 +0800

    restructured the text

--- application/routes.py
+++ application/routes.py
@@ -1,13 +1,14 @@
 import os
+import json
 from flask.helpers import send_from_directory, url_for
+from flask import render_template,Response,request,flash,send_from_directory,flash,safe_join,redirect,session
 from werkzeug.utils import redirect, secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
+from boto3.dynamodb.conditions import Key
 from application import app,dynamo
 from application.forms import LoginForm, RegisterForm,EnrollmentForm
 from application.fileserver import getFolder,allowed_file,ALLOWED_EXTENSIONS, upload_file,delete_file
-from flask import render_template,Response,request,flash,send_from_directory,flash,safe_join,redirect,session
-import json
-from boto3.dynamodb.conditions import Key
-from werkzeug.security import generate_password_hash, check_password_hash
+
 # all routes are defined here
 @app.route("/")
 @app.route("/index")
@@ -42,6 +43,7 @@ def courses(term="Spring 2019"):
     if not session.get("email"):
         return redirect(url_for('login'))
     form=EnrollmentForm()
+    # get course list from dynamoDB
     courseData=dynamo.tables['courses'].scan()['Items']
     return render_template("courses.html",courses=True,form=form,courseData=courseData,term=term)
 
@@ -51,13 +53,10 @@ def register():
         return redirect(url_for('index'))
     form=RegisterForm()
     # form.validate()
-    print("before validate on submit")
+    # print("before validate on submit")
     if form.validate_on_submit():
         email=form.email.data
-        # print("email="+email)
-        # print("clear text:"+form.password.data)
         hashed_password=generate_password_hash(form.password.data)
-        # print("hashed:"+hashed_password)
         form.password=hashed_password
         form.save()
         flash("You are successfully registered!",category="success")
@@ -69,7 +68,6 @@ def user():
     if not session.get("email"):
         return redirect(url_for('login'))
     users=dynamo.tables['users'].scan()['Items']
-    # print(users)
     return render_template("user.html",users=users,user=True)
 
 @app.route("/logout")
@@ -84,6 +82,50 @@ def confirm():
     args=request.form
     print(args)
     return render_template("confirm.html",data=args)
+
+
+@app.route("/enrollment",methods=["GET","POST"])
+def enrollment():
+    if not session.get("email"):
+        return redirect(url_for('login'))
+    form=EnrollmentForm()
+    email=session.get("email")
+    courseID=form.courseID.data
+    if courseID: # if is a enrollment submit
+        # get course detail
+        if form.validate_on_submit():
+            courseData=dynamo.tables['courses'].query(
+            KeyConditionExpression=Key('courseID').eq(courseID)
+            )['Items']
+            # update to the backend db
+            form.email=email
+            form.save()
+            return render_template("enrollment.html",title="You have successfully enrolled in the course!",enrollment=True,data=courseData)
+    else: # if is to query the enrolled courses
+        # get enrolled courseIDs
+        courseIDs=dynamo.tables['enrollment'].query(
+        KeyConditionExpression=Key('email').eq(email)
+        )['Items']
+        courseIDs=(list(map(lambda a:a['courseID'],courseIDs)))
+        enrollmentdata=[]
+        # iterate thru courseIDs and get detailed course info
+        for id in courseIDs:
+            enrollmentdata=enrollmentdata+dynamo.tables['courses'].query(
+            KeyConditionExpression=Key('courseID').eq(id)
+            )['Items']
+        # render title based on existence of results
+        title = "Here is a list of your enrolled courses" if enrollmentdata else "You haven't enrolled in any courses"
+        return render_template("enrollment.html",title=title,data=enrollmentdata,enrollment=True)
+
+@app.route("/api")
+@app.route("/api/<idx>")
+def api(idx=None):
+    courseData=dynamo.tables['courses'].scan()['Items']
+    if idx is None:
+        jdata=courseData
+    else:
+        jdata=courseData[int(idx)]
+    return Response(json.dumps(jdata),mimetype="application/json")
 
 @app.route("/fileserver",methods=["GET","POST"], strict_slashes=False)
 @app.route('/fileserver/<path:path>',methods=["GET","POST"])
@@ -118,42 +160,3 @@ def fileserver(path=''):
     else:
         print("path = "+ path)
         return render_template("fileserver.html",fileData=getFolder(safe_join(app.config["SERVING_FOLDER"],path),relative_path=path),path=path,fileserver=True)
-
-@app.route("/enrollment",methods=["GET","POST"])
-def enrollment():
-    if not session.get("email"):
-        return redirect(url_for('login'))
-    form=EnrollmentForm()
-    email=session.get("email")
-    courseID=form.courseID.data
-    if courseID: # if is a enrollment submit
-        # get course detail
-        courseData=dynamo.tables['courses'].query(
-        KeyConditionExpression=Key('courseID').eq(courseID)
-        )['Items']
-        # update to the backend db
-        form.email=email
-        form.save()
-        return render_template("enrollment.html",title="You have successfully enrolled in the course!",enrollment=True,data=courseData)
-    else: # if is to query the enrolled courses
-        courseIDs=dynamo.tables['enrollment'].query(
-        KeyConditionExpression=Key('email').eq(email)
-        )['Items']
-        courseIDs=(list(map(lambda a:a['courseID'],courseIDs)))
-        enrollmentdata=[]
-        for id in courseIDs:
-            enrollmentdata=enrollmentdata+dynamo.tables['courses'].query(
-            KeyConditionExpression=Key('courseID').eq(id)
-            )['Items']
-        title = "Here is a list of your enrolled courses" if enrollmentdata else "You haven't enrolled in any courses"
-        return render_template("enrollment.html",title=title,data=enrollmentdata,enrollment=True)
-
-@app.route("/api")
-@app.route("/api/<idx>")
-def api(idx=None):
-    courseData=dynamo.tables['courses'].scan()['Items']
-    if idx is None:
-        jdata=courseData
-    else:
-        jdata=courseData[int(idx)]
-    return Response(json.dumps(jdata),mimetype="application/json")
